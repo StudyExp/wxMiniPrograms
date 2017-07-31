4da40aa5e9fd13a955911830494f7bf078467e2b
Cyrill <lichunyang@outlook.com>
2017-07-31 14:22:25 +0800

    cc

---
+++ wx/test1/pages/list/list.js
@@ -0,0 +1,70 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msgList: [
+      { id: 1, title: "标题一", time: "2017-3-5 23:01:59", src: "../../images/1.jpg" },
+      { id: 2, title: "标题二", time: "2017-3-5 23:02:59", src: "../../images/2.jpg" },
+      { id: 3, title: "标题三", time: "2017-3-5 23:03:59", src: "../../images/3.jpg" },
+      { id: 4, title: "标题四", time: "2017-3-5 23:04:59", src: "../../images/4.jpg" }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
